f3f9a3cb94e00093cac44a2b3a870f38442723bf
lighthouse64 <rkim4425@student.dodea.edu>
2016-10-14 17:55:38 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,15 +1,11 @@
 # TSA Website
 The website for TSA.
-We will research first, so do not make any changes to these files yet. (Unless you are changing styles, basic html, etc.)  Just don't add any info yet.
+We will research first, so do not make any changes to these files yet.
 
 IMPORTANT: DO NOT MAKE CHANGES DIRECTLY TO MASTER. If you are working on anything, branch master and commit to that instead.
 If you are just adding a few lines that will not need any debugging, you can commit to master.
 
 Before you do anything, make sure you are familiar with how GitHub works. Here's a good place to start: https://guides.github.com/activities/hello-world/
-
-Hey guys! Stop deleting my php files! You can code the same exact way in the PHP files, I just need them for the include()'s.
-
-https://mission-to-mars.000webhostapp.com/
 
 It seems that google sites doesn't give you immediate raw html editing power, so I don't really want to use it.  
 
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	<meta charset="utf-8">
+        <title>Mission To Mars</title>	
+        <link rel="stylesheet" type="text/css" href="/css/style.css">
+        <link rel="stylesheet" type="text/css" href="/css/font.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    </head>
+    <body>
+        <?php include("header.php"); ?>
+	<!-- The Navigation Bar -->
+        <nav>
+            <ul>
+                <li><a href="">Privitization</a></li>
+                <li><a href="">Ongoing Solar System Exploration</a></li>
+                <li><a href="">Timeline</a></li>
+                <li><a href="">Evolution of Space Vehicles</a></li>
+                <li><a href="">Careers</a></li>
+                <li><a href="">Pros and Cons of Being Unable to Explore</a></li>
+                <li><a href="">Interviews</a></li>
+                <li><a href="">Resources</a></li>
+            </ul>
+        </nav>
+	<div id="container">
+	   <div id="header"></div>
+	   <div id="main"><h2>If you are reading this, it means that the server is now up and running!</h2></div>
+	   <div id="footer"></div>
+	</div>
+        <?php include("footer.php"); ?>
+    </body>
+</html>
