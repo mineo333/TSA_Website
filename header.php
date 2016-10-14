968b570c02f53f0ab7743398060a97496fdf2dad
DarinMao <maodarin@gmail.com>
2016-10-14 18:14:56 -0400

    Update header.php

--- header.php
+++ header.php
@@ -1,1 +1,23 @@
-<div id="header"><h1>Mission to Mars</h1></div>
+ <nav>
+
+            <ul>
+
+                <li><a href=""></a></li>
+
+                <li><a href="">Ongoing Solar System Exploration</a></li>
+
+                <li><a href=""></a></li>
+
+                <li><a href="">Evolution of Space Vehicles</a></li>
+
+                <li><a href="">Careers</a></li>
+
+                <li><a href="">Pros and Cons of Being Unable to Explore</a></li>
+
+                <li><a href="">Interviews</a></li>
+
+                <li><a href="">Resources</a></li>
+
+            </ul>
+
+        </nav>
