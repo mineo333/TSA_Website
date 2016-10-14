f39b5de951443d800559d59a8e698c4ec117e248
DarinMao <maodarin@gmail.com>
2016-10-14 18:15:40 -0400

    Update index.php

--- index.php
+++ index.php
@@ -21,24 +21,10 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     </head>
     <body>
-        
-	<!-- The Navigation Bar -->
-        <nav>
-            <ul>
-                <li><a href=""></a></li>
-                <li><a href="">Ongoing Solar System Exploration</a></li>
-                <li><a href=""></a></li>
-                <li><a href="">Evolution of Space Vehicles</a></li>
-                <li><a href="">Careers</a></li>
-                <li><a href="">Pros and Cons of Being Unable to Explore</a></li>
-                <li><a href="">Interviews</a></li>
-                <li><a href="">Resources</a></li>
-            </ul>
-        </nav>
 	<div id="container">
-	   <div id="header"></div>
+	   <?php include("header.php"); ?>
 	   <div id="main"></div>
-	   <div id="footer"></div>
+	   <?php include("footer.php"); ?>
 	</div>
         
     </body>
