70dce6ae5bda4e5f9457bcf8cfc6253e6d353457
DarinMao <maodarin@gmail.com>
2016-10-14 19:25:33 -0400

    To create new pages, copy this file to the public_html folder and edit as usual

---
+++ template.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	<!-- 
+	*******************************
+	*
+	*  Made for Technology 
+	*  Student Association
+	*
+	*
+	*  By Ethan James, Ryan Kim,
+	*  Darin Mao, Sharad Khanna,
+	*  Jason Kim, and Ben Miller.
+	*
+	*******************************
+	-->
+	<meta charset="utf-8">
+        <title>Mission To Mars</title>	
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" type="text/css" href="css/font.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    </head>
+    <body>
+	<div id="container">
+	   <?php include("header.php"); ?>
+	   <div id="main"></div>
+	   <?php include("footer.php"); ?>
+	</div>
+        
+    </body>
+</html>
