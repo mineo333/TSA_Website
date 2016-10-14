8459b130c6d3d24b1cca02c6252213f05fe5785d
DarinMao <maodarin@gmail.com>
2016-10-14 16:50:10 -0400

    Update and rename index.html to index.php
    Changed type to php and added header and footer.

--- index.php
+++ index.php
@@ -3,17 +3,17 @@
     <head>
 	<meta charset="utf-8">
         <title>Mission To Mars</title>	
-        <link rel="stylesheet" type="text/css" href="css/style.css">
-        <link rel="stylesheet" type="text/css" href="css/font.css">
+        <link rel="stylesheet" type="text/css" href="/css/style.css">
+        <link rel="stylesheet" type="text/css" href="/css/font.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     </head>
     <body>
-	    <h1>Mission To Mars</h1>
-		<h2>If you are reading this, it means that the server is now up and running!
+	<?php include("header.php"); ?>
+	<h2>If you are reading this, it means that the server is now up and running!
 	<div id="container">
 	<div id="header"></div>
 	<div id="main"></div>
-	<div id="footer"></div>
+	<?php include("footer.php"); ?>
 	</div>
     </body>
 </html>
