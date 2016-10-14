e5acc360cd0b01216926e7ccca78870e17fb41e6
DarinMao <maodarin@gmail.com>
2016-10-14 18:21:35 -0400

    Update header.php

--- header.php
+++ header.php
@@ -1,23 +1,10 @@
  <nav>
-
             <ul>
-
-                <li><a href=""></a></li>
-
                 <li><a href="">Ongoing Solar System Exploration</a></li>
-
-                <li><a href=""></a></li>
-
                 <li><a href="">Evolution of Space Vehicles</a></li>
-
                 <li><a href="">Careers</a></li>
-
                 <li><a href="">Pros and Cons of Being Unable to Explore</a></li>
-
                 <li><a href="">Interviews</a></li>
-
                 <li><a href="">Resources</a></li>
-
             </ul>
-
-        </nav>
+</nav>
