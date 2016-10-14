5ae06ccd8cfc883c911c43a7a33c3dd8526f20b1
lighthouse64 <rkim4425@student.dodea.edu>
2016-10-14 17:10:20 -0400

    Update index.php
    Nav Bar

--- index.php
+++ index.php
@@ -9,6 +9,19 @@
     </head>
     <body>
         <?php include("header.php"); ?>
+	<!-- The Navigation Bar -->
+        <nav>
+            <ul>
+                <li><a href="">Privitization</a></li>
+                <li><a href="">Ongoing Solar System Exploration</a></li>
+                <li><a href="">Timeline</a></li>
+                <li><a href="">Evolution of Space Vehicles</a></li>
+                <li><a href="">Careers</a></li>
+                <li><a href="">Pros and Cons of Being Unable to Explore</a></li>
+                <li><a href="">Interviews</a></li>
+                <li><a href="">Resources</a></li>
+            </ul>
+        </nav>
 	<div id="container">
 	   <div id="header"></div>
 	   <div id="main"><h2>If you are reading this, it means that the server is now up and running!</h2></div>
