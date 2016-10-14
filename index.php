2a377833dba45bf377e23ed3cc536d4d4ce0f239
AutogolazzoJr <1475233@gmail.com>
2016-10-14 18:08:20 -0400

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -16,8 +16,8 @@
 	-->
 	<meta charset="utf-8">
         <title>Mission To Mars</title>	
-        <link rel="stylesheet" type="text/css" href="/css/style.css">
-        <link rel="stylesheet" type="text/css" href="/css/font.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" type="text/css" href="css/font.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     </head>
     <body>
