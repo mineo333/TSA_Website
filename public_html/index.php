53f040f4aba5e5f577687b812d1a6729ee628c2b
DarinMao <maodarin@gmail.com>
2016-10-14 20:52:35 -0400

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -16,9 +16,7 @@
 	-->
 	<meta charset="utf-8">
         <title>Mission To Mars</title>	
-        <link rel="stylesheet" type="text/css" href="css/style.css">
-        <link rel="stylesheet" type="text/css" href="css/font.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+        <?php include("head.php"); ?>
     </head>
     <body>
 	<div id="container">
