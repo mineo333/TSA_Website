72c2a06fd70e0c8d586a43b095ddf5a42f52ae55
DarinMao <maodarin@gmail.com>
2016-10-15 10:42:43 -0400

    Update footer.php
    Added some basic info

--- public_html/footer.php
+++ public_html/footer.php
@@ -1,1 +1,4 @@
-<div id="footer">Footer</div>
+<div id="footer">
+	By Ethan James, Ryan Kim, Darin Mao, Sharad Khanna, Jason Kim, and Ben Miller.
+	<br>Technology Student Association 2016 Website Design
+</div>
