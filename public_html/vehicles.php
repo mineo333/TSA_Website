e8ae100f2252a8c9b56c641a20a9b53ed5692ec4
DarinMao <maodarin@gmail.com>
2016-10-15 10:30:02 -0400

    Added other pages

---
+++ public_html/vehicles.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	<!-- 
+	*******************************
+	*
+	*  Made for Technology 
+	*  Student Association
+	*
+	*
+	*  By Ethan James, Ryan Kim,
+	*  Darin Mao, Sharad Khanna,
+	*  Jason Kim, and Ben Miller.
+	*
+	*******************************
+	-->
+	<meta charset="utf-8">
+        <title>Mission To Mars</title>	
+        <?php include("head.php"); ?>
+    </head>
+    <body>
+	<div id="container">
+	   <?php include("header.php"); ?>
+	   <div id="main">This will be the space vehicle page.</div>
+	   <?php include("footer.php"); ?>
+	</div>
+        
+    </body>
+</html>
