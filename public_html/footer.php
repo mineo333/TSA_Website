bdb4b98c39a6df9c0c17c8b75b5b02398e62cea4
DarinMao <maodarin@gmail.com>
2016-10-15 11:35:23 -0400

    Ordered names by last name

--- public_html/footer.php
+++ public_html/footer.php
@@ -1,4 +1,4 @@
 <div id="footer">
-	By Ethan James, Ryan Kim, Darin Mao, Sharad Khanna, Jason Kim, and Ben Miller.
+	Ethan James, Sharad Khanna, Jason Kim, Ryan Kim, Darin Mao, and Ben Miller.
 	<br>Technology Student Association 2016 Website Design
 </div>
