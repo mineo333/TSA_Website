3ddf49316312216d466eac0a02566e9c0b7a085d
lighthouse64 <rkim4425@student.dodea.edu>
2016-10-15 08:34:27 -0400

    Update index.php
    Just put the navbar back in.  Thx for the image and styling change :)

--- public_html/index.php
+++ public_html/index.php
@@ -16,13 +16,28 @@
 	-->
 	<meta charset="utf-8">
         <title>Mission To Mars</title>	
-        <?php include("head.php"); ?>
+        <link rel="stylesheet" type="text/css" href="/css/style.css">
+<link rel="stylesheet" type="text/css" href="/css/font.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Russo+One">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     </head>
     <body>
 	<div id="container">
-	   <?php include("header.php"); ?>
+	   <img src="/images/mars.jpg" width="150" id="bannerImage">
+<nav>
+    <h1 id="bannerTitle">Mission To Mars</h1>
+    <ul>
+        <li><a href="privatization.php">Privatization</a></li>
+        <li><a href="timeline.php">Timeline</a></li>
+        <li><a href="vehicles.php">Space Vehicles</a></li>
+        <li><a href="career.php">Career Fields</a></li>
+        <li><a href="fail.php">What if we fail?</a></li>
+        <li><a href="interviews.php">Interviews</a></li>
+        <li><a href="resources.php">Resources</a></li>
+    </ul>
+</nav>
 	   <div id="main"></div>
-	   <?php include("footer.php"); ?>
+	   <div id="footer">Footer</div>
 	</div>
         
     </body>
