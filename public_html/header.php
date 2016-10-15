4b8976353bd40dd89424476f4615008c4ca28e6c
DarinMao <maodarin@gmail.com>
2016-10-14 21:28:09 -0400

    Update header.php

--- public_html/header.php
+++ public_html/header.php
@@ -1,15 +1,13 @@
-<div id="banner">
 <img src="/images/mars.jpg" width="150" id="bannerImage">
 <nav>
-            <h1 id="bannerTitle">Mission To Mars</h1>
-            <ul>
-                <li><a href="privatization.php">Privatization</a></li>
-                <li><a href="timeline.php">Timeline</a></li>
-                <li><a href="vehicles.php">Space Vehicles</a></li>
-                <li><a href="career.php">Career Fields</a></li>
-                <li><a href="fail.php">What if we fail?</a></li>
-                <li><a href="interviews.php">Interviews</a></li>
-                <li><a href="resources.php">Resources</a></li>
-            </ul>
+    <h1 id="bannerTitle">Mission To Mars</h1>
+    <ul>
+        <li><a href="privatization.php">Privatization</a></li>
+        <li><a href="timeline.php">Timeline</a></li>
+        <li><a href="vehicles.php">Space Vehicles</a></li>
+        <li><a href="career.php">Career Fields</a></li>
+        <li><a href="fail.php">What if we fail?</a></li>
+        <li><a href="interviews.php">Interviews</a></li>
+        <li><a href="resources.php">Resources</a></li>
+    </ul>
 </nav>
-</div>
