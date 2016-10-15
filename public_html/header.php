6e3af4e832d7cae64cb430fd89d54294eae7a927
DarinMao <maodarin@gmail.com>
2016-10-15 10:38:22 -0400

    Added home button

--- public_html/header.php
+++ public_html/header.php
@@ -3,6 +3,7 @@
 <nav>
     <h1 id="bannerTitle">Mission To Mars</h1>
     <ul>
+		<li><a href="/">Home</a></li>
         <li><a href="privatization.php">Privatization</a></li>
         <li><a href="timeline.php">Timeline</a></li>
         <li><a href="vehicles.php">Space Vehicles</a></li>
