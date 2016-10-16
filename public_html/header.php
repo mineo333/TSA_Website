82de465574a4e4e8fa66ba1b4252d5e2ec114399
DarinMao <maodarin@gmail.com>
2016-10-16 10:06:59 -0400

    Changed some colors

--- public_html/header.php
+++ public_html/header.php
@@ -3,14 +3,14 @@
 <nav>
     <h1>Mission To Mars</h1>
     <ul>
-		<li><a href="/" <?php if ($_SERVER["PHP_SELF"] == "/index.php"){echo "class=\"current\"";} ?>>Home</a></li>
-        <li><a href="privatization.php" <?php if ($_SERVER["PHP_SELF"] == "/privatization.php"){echo "class=\"current\"";} ?>>Privatization</a></li>
-        <li><a href="timeline.php" <?php if ($_SERVER["PHP_SELF"] == "/timeline.php"){echo "class=\"current\"";} ?>>Timeline</a></li>
-        <li><a href="vehicles.php" <?php if ($_SERVER["PHP_SELF"] == "/vehicles.php"){echo "class=\"current\"";} ?>>Space Vehicles</a></li>
-        <li><a href="career.php" <?php if ($_SERVER["PHP_SELF"] == "/career.php"){echo "class=\"current\"";} ?>>Career Fields</a></li>
-        <li><a href="fail.php" <?php if ($_SERVER["PHP_SELF"] == "/fail.php"){echo "class=\"current\"";} ?>>What if we fail?</a></li>
-        <li><a href="interviews.php" <?php if ($_SERVER["PHP_SELF"] == "/interviews.php"){echo "class=\"current\"";} ?>>Interviews</a></li>
-        <li><a href="resources.php" <?php if ($_SERVER["PHP_SELF"] == "/resources.php"){echo "class=\"current\"";} ?>>Resources</a></li>
+		<li <?php if ($_SERVER["PHP_SELF"] == "/index.php"){echo "class=\"current\"";} ?>><a href="/">Home</a></li>
+        <li <?php if ($_SERVER["PHP_SELF"] == "/privatization.php"){echo "class=\"current\"";} ?>><a href="privatization.php">Privatization</a></li>
+        <li <?php if ($_SERVER["PHP_SELF"] == "/timeline.php"){echo "class=\"current\"";} ?>><a href="timeline.php">Timeline</a></li>
+        <li <?php if ($_SERVER["PHP_SELF"] == "/vehicles.php"){echo "class=\"current\"";} ?>><a href="vehicles.php">Space Vehicles</a></li>
+        <li <?php if ($_SERVER["PHP_SELF"] == "/career.php"){echo "class=\"current\"";} ?>><a href="career.php">Career Fields</a></li>
+        <li <?php if ($_SERVER["PHP_SELF"] == "/fail.php"){echo "class=\"current\"";} ?>><a href="fail.php">What if we fail?</a></li>
+        <li <?php if ($_SERVER["PHP_SELF"] == "/interviews.php"){echo "class=\"current\"";} ?>><a href="interviews.php">Interviews</a></li>
+        <li <?php if ($_SERVER["PHP_SELF"] == "/resources.php"){echo "class=\"current\"";} ?>><a href="resources.php">Resources</a></li>
     </ul>
 </nav>
 </header>
