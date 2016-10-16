5e15ed7d4d8a18c9e7e27253e87bd160375409a9
DarinMao <maodarin@gmail.com>
2016-10-16 12:46:21 -0400

    Added image to 404 page.

--- public_html/404.php
+++ public_html/404.php
@@ -27,6 +27,7 @@
 				The requested URL <?php echo $_SERVER["REQUEST_URI"]; ?> was not found on this server
 			</header>
 			Don't give up looking, though! NASA scientists <a href="https://www.nasa.gov/press-release/nasa-confirms-evidence-that-liquid-water-flows-on-today-s-mars">found water on Mars</a> after <i>years</i> of looking
+			<br><img src="http://www.nasa.gov/sites/default/files/thumbnails/image/15-195_perspective_2.jpg" style="width:100%;">
 		   </article>
 	   </main>
 	   <?php include("../includes/footer.php"); ?>
