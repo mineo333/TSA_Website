45c5d44f835c6d9ede5cddaca25eabfd770ee51f
AutogolazzoJr <1475233@gmail.com>
2016-10-17 11:39:07 -0400

    Update head.php
    Stop making the same mistake Darin!
    There is NO slash in front of the link href! The slash sets the default directory to the root, which is okay for the website, but not the preview.

--- includes/head.php
+++ includes/head.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" type="text/css" href="/css/style.css">
-<link rel="stylesheet" type="text/css" href="/css/font.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/font.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Russo+One">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
