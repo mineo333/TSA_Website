7bfa2fc81a7669a62161f928d48e42e95dcdea94
mineo333 <sharadawsomeness@gmail.com>
2016-10-18 16:23:27 -0400

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -18,6 +18,7 @@
         <title>Mission To Mars</title>	
         <link rel="stylesheet" type="text/css" href="css/style.css">
         <link rel="stylesheet" type="text/css" href="css/font.css">
+	    <!--- JS? I already have a JS sheet that we can link up if you want? I will comment link it below. -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     </head>
     <body>
