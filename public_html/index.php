2116f71a6b32596ca0dc4ff4ccc2cb55af6a4ef7
DarinMao <maodarin@gmail.com>
2016-11-02 21:37:55 -0400

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -6,8 +6,7 @@
 		*
 		*  Made for Technology 
 		*  Student Association
-		*
-		*
+		*		*
 		*  By Ethan James, Ryan Kim,
 		*  Darin Mao, Sharad Khanna,
 		*  Jason Kim, and Ben Miller.
@@ -28,6 +27,37 @@
 			</h2>
 			<p>We'll be putting announcements here as we need them.
 			<p>PLEASE DO RESEARCH. NO MORE CODING UNTIL THIS IS FINISHED
+			<p>We might need a dropdown menu for certain things. For example, the main "career field" page would link to
+				the other main articles for each individual career fields. Similar idea for interviews.
+			<ul>
+				<h4>PROPOSED SITEMAP (BUILD AROUND THIS)</h4>
+				<li>Home</li>
+				<li>Privatization</li>
+				<li>Timeline</li>
+				<li>Space Vehicles
+					<ul>
+						<li>Manned</li>
+						<li>Unmanned</li>
+					</ul>
+				</li>
+				<li>Career Fields
+					<ul>
+						<li>Career Field One</li>
+						<li>Career Field Two</li>
+						<li>Career Field Three</li>
+						<li>Career Field Four</li>
+						<li>Career Field Five</li>
+					</ul>
+				</li>
+				<li>What if we fail?</li>
+				<li>Interviews
+					<ul>
+						<li>Interview One</li>
+						<li>Interview Two</li>
+					</ul>	
+				</li>
+				<li>Resources</li>
+			</ul>
 			<p>To Do List:
 			
 			<ul>
