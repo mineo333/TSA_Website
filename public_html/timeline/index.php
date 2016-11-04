8820956b3ce4dedad31c0a417d61ea207b7c89cf
DarinMao <maodarin@gmail.com>
2016-11-04 14:07:44 -0400

    Update timeline

--- public_html/timeline/index.php
+++ public_html/timeline/index.php
@@ -40,7 +40,9 @@
 			<h2>
 				Timeline
 			</h2>
-			This will be the interactive timeline.
+			<p>This will be the interactive timeline. I've managed to pull the data from file "data.json". The data is displayed here. Doing this is half the work; the rest is just a matter of how we're going to display the data.
+			<p>Funny story: I've spent hours working on this, and I was completely baffled as to why it didn't work. I've done this kind of work before (in my Hangman AI), and this was literally the <i>exact same thing!</i> I just figured it out today, and you know what the problem was? It was a single "." that shouldn't have been there in the data file. I had spent hours debugging the main timeline/index.php file, when I didn't even think to check the data file. I probably made that mistake while I was typing out the data file at our meeting last Thursday.
+			<p>Moral of the story: don't just check the main parts of a project when it fails. The small parts, that you "know" won't fail, might just have a little mistake in them that causes your whole project to screw up.
 			<table style="width:100%;" id="eventTable">
 				<tr>
 					<td><b>Event Name</b></td>
