9fcff29954fb51eb61ccc5b6d5b12efcc1c1f810
DarinMao <maodarin@gmail.com>
2016-11-10 16:16:31 -0500

    Update footer.php
    removed text-muted from footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
-	<footer class="footer container-fluid text-muted">
+	<footer class="footer container-fluid">
 		Website By Ethan James, Jason Kim, Ryan Kim, Darin Mao, Sharad Khanna, and Ben Miller.
 		<br>Technology Student Association 2016 Website Design
 		<div id="background-attribution">Courtesy NASA/JPL-Caltech</div>
