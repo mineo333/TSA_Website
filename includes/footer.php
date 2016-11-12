27a47ae931c2dd882743e8b90151c1b7d6600961
DarinMao <maodarin@gmail.com>
2016-11-11 21:23:53 -0500

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 	<footer class="footer container-fluid">
 		Website By Ethan James, Jason Kim, Ryan Kim, Darin Mao, Sharad Khanna, and Ben Miller.
 		<br>Technology Student Association 2016 Website Design
-		<div id="background-attribution">Courtesy NASA/JPL-Caltech</div>
+		<div id="background-attribution">Image credit: NASA/JPL-Caltech/Cornell Univ./Arizona State Univ</div>
 	</footer>
 
