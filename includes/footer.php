3de475ad8d988f8563b8036205e4f3688ce831b5
AutogolazzoJr <1475233@gmail.com>
2016-11-15 11:08:45 -0500

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 	<footer class="footer container-fluid">
-		Website By Ethan James, Jason Kim, Ryan Kim, Darin Mao, Sharad Khanna, and Ben Miller.
+		Website By Ethan James, Jason Kim, Ryan Kim, Darin Mao, and Ben Miller.
 		<br>Technology Student Association 2016 Website Design
 		<div id="background-attribution">Image credit: NASA/JPL-Caltech/Cornell Univ./Arizona State Univ</div>
 	</footer>
