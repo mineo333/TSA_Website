5d9b1fd1b6b39388c0ebf573bcf42ead489247ce
lighthouse64 <rkim4425@student.dodea.edu>
2016-11-17 16:18:57 -0500

    Update unmanned.php

--- public_html/vehicles/unmanned.php
+++ public_html/vehicles/unmanned.php
@@ -25,7 +25,16 @@
 				Unmanned Space Vehicles
 			</h2>
 		</div>
-		<p>This will be the unmanned space vehicles page
+		<p>
+		Ever since the launch of Sputnik, unmanned space vehicles have been constantly evolving.  Here is a page with some major changes that have happened over time.	
+			<p>
+			<div class="panel-heading">Sputnik</div>
+			<div class="panel-body">
+			<img class="img-responsive" src="http://nssdc.gsfc.nasa.gov/planetary/image/sputnik_asm.jpg" />
+			</div>
+			</p>
+		</p>
+	<p><h2>Cited Sources</h2></p>
 	</div>
    <?php include("../../includes/footer.php"); ?> 
     </body>
