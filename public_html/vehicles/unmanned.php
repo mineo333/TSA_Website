58589e640367f798ca483e4ebca3d502bfa9dfe2
DarinMao <DarinMao@github.com>
2016-12-11 18:14:20 -0500

    Changed unmanned vehicles to dynamically load data from JSON file
    From now on, edit space vehicles from data-unmanned.json.
    Also fixed small error in timeline.

--- public_html/vehicles/unmanned.php
+++ public_html/vehicles/unmanned.php
@@ -21,12 +21,46 @@
 				color: black;
 			}
 			div.panel-body figure {
-				float: left;
+				width: 47.5%;
 			}
-			div.panel-body .spacecraft-description {
-				float: right;
+			div.panel-body .vehicle-description {
+				width: 47.5%;
+			}
+			@media (min-width: 768px) {
+				div.panel-body figure {
+					float:left;
+				}
+				div.panel-body .vehicle-description {
+					float:right;
+				}
 			}
 		</style>
+				<script type="text/javascript">
+		<!--
+			$(function()
+			{
+				$.get("data-unmanned.json", function(data) {
+					$.each(data, function(index, event)
+					{
+						var name = event.name;
+						var description = event.description;
+						var image = event.image;
+						var caption = event.caption;
+						var displayString = "<div class=\"panel panel-default\"><div class=\"panel-heading\">";
+						displayString += name;
+						displayString += "</div><div class=\"panel-body\"><figure><img class=\"img-responsive\" src=\"";
+						displayString += image;
+						displayString += "\" /><figcaption>";
+						displayString += caption;
+						displayString += "</figcaption></figure><div class=\"vehicle-description\">";
+						displayString += description;
+						displayString += "</div></div></div>";
+						$(".vehicles").append(displayString);
+					});
+				});
+			});
+		//-->
+		</script>
     </head>
     <body>
 <?php include("../../includes/header.php"); ?>
@@ -38,16 +72,18 @@
 		</div>
 		<p>
 		Ever since the launch of Sputnik, unmanned space vehicles have constantly been evolving.  This page outlines some major changes that have happened over time.	
-			<p>
+		</p>
+		<div class="vehicles">
+		</div>
+		<!--
 			<div class="panel panel-default">
 				<div class="panel-heading">Sputnik</div>
 				<div class="panel-body">
 					<figure>
-						<img class="img-responsive"  style="height:25%; width:25%;" src="http://nssdc.gsfc.nasa.gov/planetary/image/sputnik_asm.jpg" />
+						<img class="img-responsive" src="http://nssdc.gsfc.nasa.gov/planetary/image/sputnik_asm.jpg" />
 						<figcaption> An image of the Sputnik 1 in space</figcaption>
 					</figure>
-					<div class="spacecraft-description">
-						Description of sputnik 1
+					<div class="vehicle-description">
 					</div>
 				</div>
 			</div>
@@ -55,7 +91,7 @@
 				<div class="panel-heading">Explorer I</div>
 				<div class="panel-body">
 					<figure>
-						<img class="img-responsive" style="height:25%; width:25%;" src="http://history.nasa.gov/sputnik/phot2.gif" />
+						<img class="img-responsive"src="http://history.nasa.gov/sputnik/phot2.gif" />
 						<figcaption>An image of explorer 1 flying in space</figcaption>
 					</figure>
 				</div>
@@ -64,7 +100,7 @@
 				<div class="panel-heading">Mariner 2</div>
 				<div class="panel-body">
 					<figure>
-						<img class="img-responsive" style="height:50%; width:50%;" src="http://nssdc.gsfc.nasa.gov/image/spacecraft/mariner02.gif" />
+						<img class="img-responsive" src="http://nssdc.gsfc.nasa.gov/image/spacecraft/mariner02.gif" />
 						<figcaption>An image of mariner 2 flying in space</figcaption>
 					</figure>
 				</div>
@@ -73,7 +109,7 @@
 				<div class="panel-heading">Luna 16</div>
 				<div class="panel-body">
 					<figure>
-						<img class="img-responsive" style="height:50%; width:50%;" src="http://nssdc.gsfc.nasa.gov/image/spacecraft/luna-16.jpg" />
+						<img class="img-responsive" src="http://nssdc.gsfc.nasa.gov/image/spacecraft/luna-16.jpg" />
 						<figcaption>An image of Luna 16 flying in space</figcaption>
 					</figure>
 				</div>
@@ -82,7 +118,7 @@
 				<div class="panel-heading">Hubble Telescope</div>
 				<div class="panel-body">
 					<figure>
-						<img class="img-responsive" style="height:50%; width:50%;" src="https://www.nasa.gov/sites/default/files/thumbnails/image/hubble_components_diagram.png" />
+						<img class="img-responsive" src="https://www.nasa.gov/sites/default/files/thumbnails/image/hubble_components_diagram.png" />
 						<figcaption>A diagram of the Hubble Telescope</figcaption>
 					</figure>
 				</div>
@@ -91,7 +127,7 @@
 				<div class="panel-heading">Mars Odyssey</div>
 				<div class="panel-body">
 					<figure>
-						<img class="img-responsive" style="height:50%; width:50%;" src="http://mars.jpl.nasa.gov/odyssey/images/odyssey-chart-update-br-fi.jpg" />
+						<img class="img-responsive" src="http://mars.jpl.nasa.gov/odyssey/images/odyssey-chart-update-br-fi.jpg" />
 						<figcaption>A diagram of the Hubble Telescope</figcaption>
 					</figure>
 				</div>
@@ -100,13 +136,14 @@
 				<div class="panel-heading">Curiosity Rover</div>
 				<div class="panel-body">
 					<figure>
-						<img class="img-responsive" style="height:25%; width:25%;" src="https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/pia19920_bigsky-selfie.jpg?itok=l8hfwQde" />
+						<img class="img-responsive"src="https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/pia19920_bigsky-selfie.jpg?itok=l8hfwQde" />
 						<figcaption>A picture of the Curiosity Rover on mars</figcaption>
 					</figure>
 				</div>
 			</div>
 			</p>
 		</p>
+	-->
 	<p><h2>Cited Sources</h2></p>
 	</div>
    <?php include("../../includes/footer.php"); ?> 
