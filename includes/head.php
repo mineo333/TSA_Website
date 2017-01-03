74337b6861d35422928045b67c9c68aaa96421d2
DarinMao <maodarin@gmail.com>
2017-01-02 21:06:56 -0500

    Changed mobile navbar to offcanvas

--- includes/head.php
+++ includes/head.php
@@ -1,6 +1,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Russo+One" />
 		<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css" />
+		<link rel="stylesheet" type="text/css" href="/css/bootstrap.offcanvas.min.css" />
 		<link rel="stylesheet" type="text/css" href="/css/style.css" />
 		<link rel="stylesheet" type="text/css" href="/css/font.css" />
 		<!-- IE -->
@@ -9,3 +10,4 @@
 		<link rel="icon" type="image/x-icon" href="/favicon.ico" />
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 		<script src="/js/bootstrap.min.js"></script>
+		<script src="/js/bootstrap.offcanvas.min.js"></script>
