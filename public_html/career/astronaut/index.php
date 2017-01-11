3094a1eeaa328c95d93fb31caa6775683cc1e2db
DarinMao <maodarin@gmail.com>
2017-01-10 21:28:03 -0500

    Finished astronaut career page

--- public_html/career/astronaut/index.php
+++ public_html/career/astronaut/index.php
@@ -16,6 +16,16 @@
 		<meta charset="utf-8">
 		<title>Mission To Mars</title>	
 <?php include("../../../includes/head.php"); ?>
+	<style type="text/css">
+		figure {
+			float: right;
+			width: 30%;
+			margin: 8px;
+			padding: 8px;
+			border-radius: 5px;
+			background-color: white;
+		}
+	</style>
     </head>
     <body>
 <?php include("../../../includes/navbar.php"); ?>
@@ -25,7 +35,61 @@
 				Astronauts
 			</h2>
 		</div>
-		<p>This will be the first career field
+		<p>Among all of the people involved in space exploration, astronauts are the most well known. The reason for this is quite obvious; astronauts are the people who actually go to space. </p>
+		<figure>
+			<img class="img-responsive" src="/images/brucemccandless.jpg">
+			<figcaption>
+				Astronaut Bruce McCandless &#8545; using a Manned Maneuvering Unit (MMU) on shuttle mission STS-41-B in 1984. 
+				<br>By NASA [Public domain], <a href="https://commons.wikimedia.org/wiki/File%3AAstronaut-EVA.jpg">via Wikimedia Commons</a>
+			</figcaption>
+		</figure>
+		<section>
+			<header><h3>Jobs</h3></header>
+			<p>Astronauts perform actions that require human input, and cannot be done automatically by computers and machines. They also monitor automated actions, to make sure that they are being done correctly. As of today (2017) there are not many tasks that computers can do just as well as humans; computers just aren't advanced enough. Having humans operating machinery makes designing the spacecraft much easier. Even though humans aren't as fast or accurate, and make more mistakes, we can handle those mistakes much better, should they arise. </p>
+			<p>Humans can't survive very well on their own in space. One of the jobs of the astronauts is to monitor the life support systems, to make sure that they are working properly. Without life support systems, the astronauts would not live very long. </p>	
+		</section>
+		<section>
+			<header><h3>Spacesuit</h3></header>
+			<p>One of the identifying features of an astronaut is the spacesuit. As children we all learn about astronauts, and there is always a picture of an astronaut, in a spacesuit, doing a spacewalk. These space suits are worn for many reasons. The first is temperature. Temperature is space can vary between -250 &deg;F (-156.67 %deg;F) and 250 &deg;F (121.11 &deg;F). The spacesuit will regulate the temperature, and keep it at a comfortable level. Spacesuits also supply oxygen and water. Without these two critical supplies, the astronaut would not live very long. The spacesuit also protects the astronauts against "space dust", radiation, and bright sunlight. </p>
+			<p>Spacesuits have several pieces. The Hard Upper Torso covers the (you guessed it) torso. The arm assembly covers the (again) arms. The helmet and Extravehicular Visor Assembly protect the astronauts head without obstructing his/her view. The Lower Torso Assembly covers the astronaut's legs and feet. </p>
+			<p>Underneath the spacesuit there is a Liquid Cooling and Ventilation system. Tubes are woven into the suit, which carry water and cool the astronaut. </p>
+			<p>You may have noticed a "backpack" in some pictures of astronauts. This backpack carries the Primary Life Support Subsystem. It supplies the oxygen, and also removes the carbon dioxide. It also has a fan and water tank to move oxygen and supply cooling water, respectively. The backpack also provides electricity. </p>
+			<p>The suit also has a Simplified Aid for Extravehicular Activity Rescue (SAFER) on the back. The SAFER consists of several small thruster jets that allow the astronaut to fly back to the space station, should he/she become separated. </p>
+		</section>
+		<figure>
+			<img class="img-responsive" src="/images/yurigagarin.jpg">
+			<figcaption>
+				Yuri Gagarin, the first person to go to space.
+				<br>By Arkiv: Sydsvenskan (sydsvenskan.se) [Public domain], <a href="https://commons.wikimedia.org/wiki/File%3AGagarin_in_Sweden.jpg">via Wikimedia Commons</a>
+			</figcaption>
+		</figure>
+		<section>
+			<header><h3>Sleep</h3></header>
+			<p>Astronauts are humans, too, and they need to sleep, just like on Earth. Astronauts are scheduled for eight hours of sleep each day. Astronauts sleep in small cabins, which are large enough to hold one person. Because there is no (or at least very little) gravity in space, they must sleep in sleeping bags attached to the cabin. This way they will not float around and bump into things. </p>
+		</section>
+		<section>
+			<header><h3>Exercise</h3></header>
+			<p>The conditions of space are very different from those on Earth. These differences can cause severe health effects. To prevent these effects, astronauts must exercise every day. On average, astronauts exercise two hours every day. </p>
+			<p>The exercise equipment astronauts use is very different from the types used on Earth. This is done to account for the difference in gravity, among other things. </p>
+		</section>
+		<section>
+			<header><h3>Fun</h3></header>
+			<p>Like most people on Earth, astronauts get weekends off. Even if it's not a weekend, crew members can have fun like we do. They can watch movies, read books, play games, etc. Fun is an essential part of life, and without it life is very boring. Having fun gives the astronauts a break from work, keeping them interested, active, and focused in their job. And with a job as important and as dangerous as theirs, staying focused is more important than ever. </p>
+		</section>
+		<section>
+			<header><h3>Personal Hygiene</h3></header>
+			<p>Astronauts take care of personal hygiene in the same ways that we do on Earth: wash their hair, brush their teeth, use the restroom, etc. But, because of the different conditions in space, these routines are also different in some ways. </p>
+			<p>Astronauts wash their hair with a "rinseless" shampoo.</p>
+			<p>Many astronauts have a personal hygiene kit, containing the personal hygiene items the astronaut has chosen to take to space. </p>
+			<p>Toilets have vacuums, that suck waste into the commode. This is done to keep waste from floating around. That would not be a pleasant experience. </p>
+		</section>
+		<section>
+			<header><h3>Sources</h3></header>
+			<ul class="sources">
+				<li><a href="https://www.nasa.gov/audience/foreducators/stem-on-station/dayinthelife#.VD_TifldU1I" target="_blank">https://www.nasa.gov/audience/foreducators/stem-on-station/dayinthelife#.VD_TifldU1I</a></li>
+				<li><a href="https://www.nasa.gov/audience/forstudents/5-8/features/nasa-knows/what-is-a-spacesuit-58.html" target="_blank">https://www.nasa.gov/audience/forstudents/5-8/features/nasa-knows/what-is-a-spacesuit-58.html</a></li>
+			</ul>
+		</section>
 	</div>
    <?php include("../../../includes/footer.php"); ?> 
     </body>
