1140b9b15957e7ef549dd4294d4ac8ecdf9a2401
DarinMao <maodarin@gmail.com>
2017-01-12 15:13:04 -0500

    Add manned spacevehicles page

--- public_html/vehicles/manned/index.php
+++ public_html/vehicles/manned/index.php
@@ -16,6 +16,50 @@
 		<meta charset="utf-8">
 		<title>Mission To Mars</title>	
 <?php include("../../../includes/head.php"); ?>
+		<style type="text/css">
+			div.panel {
+				color: black;
+			}
+			div.vehicle-description {
+				margin-top: 1%;
+			}
+			@media (min-width: 768px) {
+				div.panel-body figure {
+					width: 30%;
+					float: left;
+				}
+				div.panel-body div.vehicle-description {
+					width: 65%;
+					float: right;
+					margin-top: 0px;
+				}
+			}
+		</style>
+				<script type="text/javascript">
+		<!--
+			$(function()
+			{
+				var data = <?php include("../../../data/manned.json"); ?>;
+				$.each(data, function(index, event)
+				{
+					var name = event.name;
+					var description = event.description;
+					var image = event.image;
+					var caption = event.caption;
+					var displayString = "<div class=\"panel panel-default\"><div class=\"panel-heading\">";
+					displayString += name;
+					displayString += "</div><div class=\"panel-body\"><figure><img class=\"img-responsive\" src=\"";
+					displayString += image;
+					displayString += "\" /><figcaption>";
+					displayString += caption;
+					displayString += "</figcaption></figure><div class=\"vehicle-description\">";
+					displayString += description;
+					displayString += "</div></div></div>";
+					$(".vehicles").append(displayString);
+				});
+			});
+		//-->
+		</script>
     </head>
     <body>
 <?php include("../../../includes/navbar.php"); ?>
@@ -25,7 +69,12 @@
 				Manned Space Vehicles
 			</h2>
 		</div>
-		<p>This will be the manned space vehicle page
+		<p>
+		
+		</p>
+		<div class="vehicles">
+		</div>
+	<p><h2>Cited Sources</h2></p>
 	</div>
    <?php include("../../../includes/footer.php"); ?> 
     </body>
