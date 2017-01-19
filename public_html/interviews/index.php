8a48f8f5ad7842b3d67ef64c67a424322b9d3950
DarinMao <maodarin@gmail.com>
2017-01-19 15:06:34 -0500

    Removed Q:s

--- public_html/interviews/index.php
+++ public_html/interviews/index.php
@@ -27,8 +27,8 @@
 		</div>
 		<p>In order to gain a deeper understanding of the current situation on the privatization and exploration of Mars, as well as the rest of space, we contacted and interviewed two experts in the field of space exploration. We asked them the following questions:</p>
 		<ol>
-			<li>Q: Why do you believe people will reach Mars?</li>
-			<li>Q: What will people do once they reach Mars?</li>
+			<li>Why do you believe people will reach Mars?</li>
+			<li>What will people do once they reach Mars?</li>
 		</ol>
 	</div>
 	<?php include("../../includes/footer.php"); ?>    
