73e830ee1a366b5bedca7ec174a1c1a4a3ff4547
DarinMao <maodarin@gmail.com>
2017-01-19 21:00:37 -0500

    Finished privatization page (thanks Ben\!)

--- public_html/privatization/index.php
+++ public_html/privatization/index.php
@@ -16,6 +16,24 @@
 		<meta charset="utf-8">
 		<title>Mission To Mars</title>	
 		<?php include("../../includes/head.php"); ?>
+		<style type="text/css">
+			figure {
+				width: 100%;
+				background-color: white;
+				border-radius: 5px;
+				padding: 8px;
+			}
+			@media (min-width: 768px) {
+				figure {
+					float: right;
+					width: 30%;
+					margin: 8px;
+				}
+				figure.left {
+					float: left;
+				}
+			}
+		</style>
     </head>
     <body>
 	<?php include("../../includes/navbar.php"); ?>
@@ -25,7 +43,66 @@
 				Privatization
 			</h2>
 		</div>
-		<p>This will be the privatization page.
+		<p>Every since NASA has had its budget cut, it has begun to leave the picture. China is a little too late to the party (they first got a man into space
+		in 2003), and the European Space Agency, well, European governments have more to worry about than space. In fact, that’s something that almost every 
+		government on the planet suffers from: they have too much on their plate. With that in mind, private companies have begun to take center stage. 
+		SpaceX, created and headed by Elon Musk, has good odds, seeing as they’ve been doing missions for NASA and other companies since October 2008. The Mars 
+		One team has extensive plans to use established aerospace companies (like SpaceX) to create and develop the necessary technologies for a martian 
+		habitat. Another contender is this department is The Mars Society, who already has a simulated Mars habitat set up in the Utah desert. It remains to 
+		be seen which one of these companies will make it to the red planet first.</p>
+		<figure>
+			<img class="img-responsive" src="/images/falcon9.jpg">
+			<figcaption>
+				The first stage of the Falcon 9 lands on December 21, 2015. 
+				<div class="image-attribution">
+					By SpaceX [<a href="http://creativecommons.org/publicdomain/zero/1.0/deed.en">CC0</a> or 
+					<a href="http://creativecommons.org/publicdomain/zero/1.0/deed.en">CC0</a>], 
+					<a href="https://commons.wikimedia.org/wiki/File%3AORBCOMM-2_First-Stage_Landing_(23271687254).jpg" target="blank">via Wikimedia Commons</a>
+				</div>
+			</figcaption>
+		</figure>
+		<p>Founded in 2002 and led by Elon Musk, the 21st richest man on the planet, SpaceX made headlines when it spearheaded the attempt by NASA to privatize
+		the space industry when its Falcon 9 rocket blasted off from Cape Canaveral, Florida on October 8th, 2012, the first supply run by a private company in 
+		the history of NASA. Since then, they’ve completed another 10 for NASA and 17 for other companies and non-American government agencies, creating the 
+		longest list of launches of any space company currently in existence. When it comes to Mars, they’ve made clear their goal to get there, creating a design 
+		for an Interplanetary Transport System, along with designing a rocket that allows for recovery of its boosters, essential when it comes to the limited 
+		supplies of a Mars mission. In addition, there Mars Colonial Transporter is due out by the end of the year, and Musk has mentioned that they have a space 
+		suit in the works as well. With a drive to make it their, and the funds, to boot, they’re widely considered to be the top prospect in this growing industry.</p>
+		<figure class="left">
+			<img class="img-responsive" src="/images/marsone.jpg">
+			<figcaption>
+				A model of the proposed Mars One colony 
+				<div class="image-attribution">
+					By Per-Olof Forsberg [<a href="https://creativecommons.org/licenses/by/2.0/">CC BY 2.0</a>], 
+					<a href="https://www.flickr.com/photos/perolofforsberg/21418170014" target="blank">via Flickr</a>
+				</div>
+			</figcaption>
+		</figure>
+		<p>Mars One, while relatively new on the scene (est. 2011) already has a projected launch date for its first unmanned rover (2026) and for its first 
+		launch of a human crew to Mars (2031). Their timeline includes the launch of a satellite in 2024, the launch of cargo missions and set-up in 2029 and 
+		2030, respectively, and the next giant leap for mankind in 2032. In fact, there hope is for a human crew to begin training full-time by 2017 and 
+		continue until 2031, when they will launch for the red planet, allowing the crew to get used to the long period of isolation that they will endure 
+		while on their mission. So far, they have plans for the departure of a total of 3 crews, though they have made it clear that if they do succeed, new 
+		crews will begin to launch every 26 months. When it comes to astronaut safety, every time a crew is launched, the supplies and habitats for the next 
+		crew will be launched a few months later, giving a high level of redundancy in case of machine failure or other unforeseen circumstances. Of all the 
+		companies with a clear blueprint for Martian colonization, this group definitely takes 1st place.</p>
+		<figure>
+			<img class="img-responsive" src="/images/marssociety.jpg">
+			<figcaption>
+				The Flashline Mars Arctic Research Station (FMARS) Habitat on Devon Island in Nunavut, Canada
+				<div class="image-attribution">
+					By Brian Shiro (Own work) [<a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY-SA 3.0</a>], 
+					<a href="https://commons.wikimedia.org/wiki/File%3AFMARS_2009_hab.jpg">via Wikimedia Commons</a>
+				</div>
+			</figcaption>
+		</figure>
+		<p>The Mars Society, created in August of 1998, is an organization dedicated to the colonization of the red planet. Unlike other teams and research 
+		groups vying for the opportunity to put a human on Mars, they actually have a plan on bringing the astronauts back to Earth. To accomplish this, they’ve 
+		set up a hypothetical system in which the explorers utilize the martian surface to develop the fuel needed for the return trip, and exploit the orbits of 
+		the two planets to vastly reduce the time and energy needed for a return trip by a scale of six. Another factor on their side, they’ve already got a test 
+		site, set up in the south of Utah, and have plans for a 1-year simulated mission in the Canadian high arctic, christened the “Mars Arctic 365 Mission”. 
+		As their president Robert Zubrin pointed out during a press conference: <em>“We are much closer today to being able to send humans to Mars than we were to 
+		being able to send men to the Moon in 1961, and we were there eight years later. Given the will, we could have humans on Mars within a decade.”</em></p>
 	</div>
 	<?php include("../../includes/footer.php"); ?>    
     </body>
