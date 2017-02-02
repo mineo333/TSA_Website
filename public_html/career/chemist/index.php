1b13cfae8e8b22a9ee9e569e34ee789770c9d60f
Benjamin Miller <benjamin@amillers.net>
2017-02-02 15:43:00 -0500

    Update index.php

--- public_html/career/chemist/index.php
+++ public_html/career/chemist/index.php
@@ -25,7 +25,33 @@
 				Career Field four
 			</h2>
 		</div>
-		<p>This will be the fourth career field
+		<p>
+
+A general idea of what a chemist does, and the background they might come in with when they join NASA.
+	In the growing field of aerospace, the future chemists and scientists will likely not be performing any of the experiments. However, robots and landers will be doing the dirty work, while scientists in control stations and offices will instead be designing new and innovative tests to be done on Martian soil. 
+	Probably the most famous (or infamous) experiments done in this fashion was by the 1976 Viking lander. It scooped up a sample of soil, then added a solution of amino acids and other nutrient molecules that were radiolabelled with C14, an experiment drawn-up by NASA chemists. After a few hours, the sample began gave off CO2 radiolabelled with the same Carbon, indicating that the molecules from Earth were being broken down in by something in the soil, likely something alive.
+	Unfortunately for this experiment, it probably wasn’t all it was cracked up to be. When  more organic molecules were added a week later, they failed create another spike in CO2, something that should have happened if the purported life forms had multiplied. As of today, scientists believe that inorganic salts in the Martian soil, such as superoxide containing O2 ions, were responsible for the observed oxidation, as opposed to biological action.
+	In the future, chemists will likely be of high demand in potential Martian colonies. Besides being of high demand for performing experiments, they could provide a lifesaving support in case of potential natural disasters or chemical contaminants. Whatever the reason, chemists will sure to be vital in any and all future settlements on the Red Planet.
+
+
+Words from a Chemist
+“One exciting technology I helped create is an energy source that can be printed on paper using special ink. NASA can use this energy-efficient light on space missions or even to power space vehicles. Tiny molecules printed within the ink work as semiconductors, or materials that allow energy to be transferred through them. These devices are lightweight, very affordable, and generate little heat. Because all the energy is stored in the ink, the light source can be printed on normal paper, opening up all sorts of new possibilities: providing light on space missions, energy for solar cells to power entire vehicles, or even light for your home!
+The challenge of discovering new things is the most exciting part of my job. I get to advise NASA about which technologies might be the most helpful for the space program. I also work with private companies to help bring unique technologies to NASA.
+I was curious about how things around me worked from an early age. I grew up in a neighborhood full of distracting influences. Because I wanted to distance myself from these influences and be successful in life, I joined the Army and worked as a meteorological observer. That experience deepened my interest in science and discovery.
+When I left the military, I earned advanced degrees in physical chemistry. I taught chemistry at the university level and worked in private industry as a research chemist before joining NASA. I came to NASA because sending humans into space and taking on challenges that had never been solved before intrigued me and fed my passion for discovery.
+At NASA, I have worked as a research chemist, as the Chief Scientist for Physical Chemistry, and as the mission scientist for the first United States Microgravity Laboratory Spacelab mission, which flew aboard the space shuttle in 1992. I'm now the Chief Scientist in the Engineering Technical Management Office, where I help manage the development and discovery of new technologies for NASA and society. Much of my work in chemistry at NASA has revolved around how different phases of materials, such as clouds or foams, interact.
+Plenty of opportunities exist in materials and chemistry research leading to exciting breakthroughs in a range of technologies beneficial to NASA and the nation.
+With NASA's renewed focus on research and technology development, the future is bright! I look forward to helping NASA develop even more groundbreaking technologies to enhance the future for all of us. We need young people who are interested in math, science, and engineering to help us with new and exciting space missions. It will be a thrilling journey!”
+
+
+
+
+Citations:
+https://www.nasa.gov/centers/marshall/about/marshallfaces/frazier.html
+http://www.cheminst.ca/magazine/feature-story/chemistry-mars
+
+
+
 	</div>
    <?php include("../../../includes/footer.php"); ?> 
     </body>
